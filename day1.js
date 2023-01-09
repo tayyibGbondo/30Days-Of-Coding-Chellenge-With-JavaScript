f052386ec4bbb1622ea505520664b0880de45043
tayyibcreation <111245864+tayyib232@users.noreply.github.com>
2023-01-09 16:28:18 +0000

    strings

---
+++ day1.js
@@ -0,0 +1,37 @@
+//This a single line comment
+/*this a multi line comment */
+
+/*
+Day one of learning JavaScript
+
+------Linking Your Js file with HTML
+------Preparing The text editor (Standard Lient ==> that highlight error on you code and,  standard formatting that format you code the standard way. )
+------ The console (talk about of the console work)
+ */
+
+/*
+-----Data Types In JavaScript -----
+
+string ==> represent a text in javascript
+number
+null
+undefined
+boolean
+bigInt
+symbol
+
+*/
+
+//String
+console.log("Javascript is easier than i taught");
+
+//Handling strings with apostrophes
+console.log("i'm a developer");
+console.log("i'am 19  years old");
+console.log(`i'am using template strings to handle the apostrophes`); //template strings
+
+//Adding strings Together
+//string can be added together using the + operator
+//this process is also called string concatenation
+console.log("super" + "man");
+console.log("1" + "1"); //give us a string of 11
