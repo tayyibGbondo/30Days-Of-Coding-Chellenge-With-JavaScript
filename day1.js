cf85c1d924b570ee54cce75439947f61524006a0
tayyibcreation <111245864+tayyib232@users.noreply.github.com>
2023-01-09 17:25:38 +0000

    Exercise done and subbmited

--- day1.js
+++ day1.js
@@ -56,3 +56,38 @@ console.log(22 % 5);
 //Booleans are very much like light switches. They can only be “switched on” (true) or “switched off” (false).
 console.log(true);
 console.log(false);
+
+/*
+Exercise
+Prepare yourself for the next lesson by creating numbers, strings and booleans. Try the following:
+
+
+1. Create a String and console.log it.
+2. Add two strings together.
+3. Create a Number and console.log it.
+4. Add 27 and 73.
+5. Subtract 30 from 50.
+6. Multiply 5 and 10.
+7. Divide 100 by 10.
+8. Get the remainder of 500 divided by 3.
+9. Create a Boolean and console.log it.
+*/
+
+//Exercise Answer 1
+console.log("I will be good at JS someday");
+//Exercise Answer 2
+console.log("super" + "man");
+//Exercise Answer 3
+console.log(232);
+//Exercise Answer 4
+console.log(27 + 73);
+//Exercise Answer 5
+console.log(50 - 30);
+//Exercise Answer 6
+console.log(5 * 10);
+//Exercise Answer 7
+console.log(100 / 10);
+//Exercise Answer 8
+console.log(500 % 3);
+//Exercise Answer 9
+console.log(true);
