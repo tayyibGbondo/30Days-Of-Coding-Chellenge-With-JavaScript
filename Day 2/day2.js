ceaea2cc3d660aa78fd168235ebaaa5b7fb1114c
tayyibGbondo <gbondotahib72@gmail.com>
2023-01-10 02:31:13 +0000

    variable deceliearation

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -2,3 +2,37 @@
 DAY 2 OF LEARNING JAVASCRIPT
 
  */
+
+//Variable Deceleration
+const initialApple = 4;
+console.log(initialApple);
+
+//
+
+const initialApples = 4;
+applesToStore = 27;
+const totalApple = initialApples + applesToStore;
+console.log(totalApple);
+
+//reassigning a variable
+let myName = "tayyib gbondo";
+myName = "mohamed";
+console.log(myName);
+
+/*
+Exercise
+
+Declare a string with const.
+Do these:
+Declare a variable with let. Call this variable sum. Set it to 0.
+Declare two more variables, num1 and num2. Set these to 300 and 50.
+Reassign sum with the sum of num1 and num2.
+
+*/
+
+//Exercise 1 Answer
+let sum = 0;
+let sum1 = 300;
+let sum2 = 50;
+sum = sum1 + sum2;
+console.log(sum);
