84963d63532d6b9b2c6eb41fd7e3222befd863aa
tayyibGbondo <gbondotahib72@gmail.com>
2023-01-10 23:48:09 +0000

    call back function

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -36,3 +36,60 @@ let sum1 = 300;
 let sum2 = 50;
 sum = sum1 + sum2;
 console.log(sum);
+
+//Function
+//A function is a block of code that execute a task in specific order
+
+function sayHello() {
+  console.log("Hello World!");
+}
+sayHello();
+
+//Function with parameter
+function myFunction(param1, param2, param3) {
+  console.log("First Parameter " + param1);
+  console.log("Second Parameter " + param2);
+  console.log("Third Parameter " + param3);
+}
+myFunction("myName", "myAge", "myContact");
+
+//The Return Statement
+
+function get2() {
+  console.log("i am getting another 1");
+  return "i am getting 2";
+}
+const result = get2();
+console.log(result);
+
+//
+function add2(num) {
+  return num + 5;
+}
+const theR = add2(5);
+console.log(theR);
+
+//expression
+const expres = (2 + 3, 10);
+console.log(expres);
+
+//Calling a Function in an expression
+function summ(nn) {
+  return nn + nn;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+const outPut = summ(multiply(2, 3), 10);
+console.log(outPut);
+
+//Passing a function inside of another function
+function three() {
+  return 6;
+}
+
+function add2(callback) {
+  return callback + three();
+}
+console.log(add2(9));
